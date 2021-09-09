90cf870764b395e5c30670accd674068cca79e62
akhiljachub <akhiljacob98@gmail.com>
2021-09-09 15:28:25 +0100

    Do model testing
    User and bookreview model testing.
    Gemfile changed.

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -1,7 +1,4 @@
 class BookReview < ApplicationRecord
-  self.table_name = "book_reviews"
-
-  validates :book_id, presence: true
+  validates_presence_of :book_id, :user_id
   validates :body, presence: true, length: { minimum: 3 }
-  validates :user_id, presence: true
 end
