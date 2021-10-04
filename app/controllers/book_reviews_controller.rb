9d197ada97c5a7955e17bbbf26663e7b36456075
akhiljachub <akhiljacob98@gmail.com>
2021-10-04 17:35:01 +0100

    Update first book review

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -18,7 +18,7 @@ class BookReviewsController < ApplicationController
     @book_review.user_id = current_user.id
 
     if @book_review.save
-      UserMailer.with(user: @user).welcome_email.deliver_now
+      UserMailer.with(user: @user).first_review.deliver_now
       redirect_to @book_review, notice: "Book review created."
     else
       render :new
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def welcome_email
+  def first_review
     @user = params[:user]
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Congratulations on your first review!')
