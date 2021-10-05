25da00b29de00aa5f3182bdf3e1a7c7b5e49dcd0
akhiljachub <akhiljacob98@gmail.com>
2021-10-05 13:08:15 +0100

    Update new user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  after_create :send_email!
+
+  def send_email!
+    UserMailer.with(user: @user).welcome_email(id).deliver_now
+  end
 end
